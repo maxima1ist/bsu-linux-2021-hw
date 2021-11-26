a2732120d2abe503ca51a26eadf92e39bb3e1225
Maxim Stepura <48197478+maxima1ist@users.noreply.github.com>
2021-11-26 12:17:27 +0300

    Update nonsyscalls.c

--- speed_syscall_test/nonsyscalls.c
+++ speed_syscall_test/nonsyscalls.c
@@ -6,30 +6,29 @@
 #define M 200
 
 void dumbSwapStr() {
-	char str[2 * M];
-	for (size_t i = 0; i < M; ++i) {
-		char temp = str[i];
-		str[i] = str[2 * M - i - 1];
-		str[2 * M - i - 1] = temp;
-	}
+    char str[2 * M];
+    for (size_t i = 0; i < M; ++i) {
+        char temp = str[i];
+        str[i] = str[2 * M - i - 1];
+        str[2 * M - i - 1] = temp;
+    }
 }
 
 int main() {
 
-  	struct timeval current_time;
+    struct timeval current_time;
 
-  	gettimeofday(&current_time, NULL);
-  	int start_time = current_time.tv_usec;
+    gettimeofday(&current_time, NULL);
+    int start_time = current_time.tv_usec;
 
-  	for (size_t i = 0; i < N; ++i) {
-	  	dumbSwapStr();
-	}
+    for (size_t i = 0; i < N; ++i) {
+        dumbSwapStr();
+    }
 
-  	gettimeofday(&current_time, NULL);
-  	int end_time = current_time.tv_usec;
+    gettimeofday(&current_time, NULL);
+    int end_time = current_time.tv_usec;
 
-  	printf("non syscalls time is %d micro seconds\n", end_time - start_time);
-
+    printf("non syscalls time is %d micro seconds\n", end_time - start_time);
 
     return 0;
 }
