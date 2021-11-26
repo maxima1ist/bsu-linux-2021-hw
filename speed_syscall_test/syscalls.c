bb9523d56a0bbd3413fab7b7d0582fa2f1759f49
Maxim Stepura <48197478+maxima1ist@users.noreply.github.com>
2021-11-26 12:16:37 +0300

    Update syscalls.c

--- speed_syscall_test/syscalls.c
+++ speed_syscall_test/syscalls.c
@@ -5,21 +5,19 @@
 #define N 100
 
 int main() {
+    struct timeval current_time;
+    
+    gettimeofday(&current_time, NULL);
+    int start_time = current_time.tv_usec;
 
-  	struct timeval current_time;
-
-  	gettimeofday(&current_time, NULL);
-  	int start_time = current_time.tv_usec;
-
-	for (size_t i = 0; i < N; ++i) {
-		write(1, "", 0);
-	}
-
-  	gettimeofday(&current_time, NULL);
-  	int end_time = current_time.tv_usec;
-
-  	printf("syscalls time is %d micro seconds\n", end_time - start_time);
-
+    for (size_t i = 0; i < N; ++i) {
+        write(1, "", 0);
+    }
+	
+    gettimeofday(&current_time, NULL);
+    int end_time = current_time.tv_usec;
+	
+    printf("syscalls time is %d micro seconds\n", end_time - start_time);
 
     return 0;
 }
